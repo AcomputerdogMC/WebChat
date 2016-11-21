6ebc5fcb7a9cc97e22bc8bfc3fbf1c660b43ab20
chris <acomputerdog@gmail.com>
2016-11-21 17:17:48 -0500

    Add chat timeout

--- src/net/acomputerdog/webchat/net/handler/SendHandler.java
+++ src/net/acomputerdog/webchat/net/handler/SendHandler.java
@@ -10,12 +10,17 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.InetSocketAddress;
 import java.net.URLDecoder;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.logging.Logger;
 
 public class SendHandler extends WebHandler {
     private final Server server;
     private final Logger logger;
     private final PluginWebChat plugin;
+
+    private final Map<InetSocketAddress, Timeout> timeouts = new HashMap<>();
 
     public SendHandler(WebServer server, Server mc, Logger logger, PluginWebChat plugin) {
         super(server);
@@ -27,12 +32,17 @@ public class SendHandler extends WebHandler {
     @Override
     public void handleExchange(HttpExchange exchange) throws IOException {
         try {
+            clearTimeouts();
             if (!exchange.getRequestMethod().equals("POST")) {
                 sendResponse(exchange, "<p>405 Method not allowed: only POST is accepted.</p>", 405);
                 return;
             }
             if (!"application/x-www-form-urlencoded".equals(exchange.getRequestHeaders().get("Content-Type").get(0))) {
                 sendResponse(exchange, "<p>406 Not acceptable: wrong Content-Type.</p>", 406);
+                return;
+            }
+            if (!checkAndUpdateTimeout(exchange.getRemoteAddress())) {
+                sendResponse(exchange, "<p>429 Too many requests: please wait to send multiple messages.</p>", 429);
                 return;
             }
             InputStream body = exchange.getRequestBody();
@@ -63,6 +73,32 @@ public class SendHandler extends WebHandler {
         }
     }
 
+    private void clearTimeouts() {
+        //this makes me uncomfortable
+        for (Iterator<Map.Entry<InetSocketAddress, Timeout>> iterator = timeouts.entrySet().iterator(); iterator.hasNext(); ) {
+            //loop through each entry in the set
+            Map.Entry<InetSocketAddress, Timeout> entry = iterator.next();
+            //if it has finished then remove it
+            if (entry.getValue().finished) {
+                iterator.remove();
+            }
+        }
+    }
+
+    private boolean checkAndUpdateTimeout(InetSocketAddress addr) {
+        boolean canSend = true;
+
+        Timeout timeout = timeouts.get(addr);
+        if (timeout != null && !timeout.finished) {
+            canSend = false;
+        } else {
+            timeout = new Timeout(PluginWebChat.CHAT_DELAY);
+            timeouts.put(addr, timeout);
+        }
+
+        return canSend;
+    }
+
     private void addChat(InetSocketAddress addr, String decoded) {
         String line = filterChat(decoded);
         String address = addr.getAddress().getHostAddress();
@@ -83,5 +119,36 @@ public class SendHandler extends WebHandler {
 
     private String hashIP(String addr) {
         return "WEB/" + Integer.toHexString(addr.hashCode()).toUpperCase();
+    }
+
+    private class Timeout {
+        private boolean finished = false;
+
+        private Timeout(long time) {
+            Thread thread = new Thread(() -> {
+                //time remaining as of now
+                long remaining = time;
+                //time that the current loop started
+                long start = System.currentTimeMillis();
+
+                //sleep for time milliseconds
+                do {
+                    try {
+                        Thread.sleep(remaining);
+                        remaining = 0;
+                    } catch (InterruptedException ignored) {
+                        long diff = System.currentTimeMillis() - start;
+                        remaining -= diff;
+                        start = System.currentTimeMillis();
+                    }
+                } while (remaining > 0);
+
+                finished = true;
+            });
+
+            //daemon thread so that we don't stop JVM
+            thread.setDaemon(true);
+            thread.start();
+        }
     }
 }
